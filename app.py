0c5ec71aed1bb93b652b0accea57137b247b2bca
AshazA <94416376+ashaza13@users.noreply.github.com>
2023-01-07 07:53:45 -0500

    Updated more styles and added more elements

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import os
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
-from helpers import find_similar
+from helpers import find_similar, get_trending, get_upcoming
 from datetime import datetime
 
 # Configure application 
 app = Flask(__name__)
+
+DATE_UNFORMATTED = '%Y-%m-%d'
+DATE_FORMATTED = '%B %d, %Y'
 
 # ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -19,16 +22,30 @@ app.config["SESSION_TYPE"] = "filesystem"
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("index.html")
+        popular = get_trending()
+        for i in range(len(popular)):
+            if popular[i].release_date != '':
+                date_object = datetime.strptime(popular[i].release_date, DATE_UNFORMATTED)
+                string_date = date_object.strftime(DATE_FORMATTED)
+                popular[i].release_date = string_date
+        
+        upcoming = get_upcoming()
+        for i in range(len(upcoming)):
+            if upcoming[i].release_date != '':
+                date_object = datetime.strptime(upcoming[i].release_date, DATE_UNFORMATTED)
+                string_date = date_object.strftime(DATE_FORMATTED)
+                upcoming[i].release_date = string_date
+
+
+        return render_template("index.html", popular=popular, upcoming=upcoming)
     else:
         # Find a list of similar movies
         result = find_similar(request.form.get("movie"))
 
-        print(result)
         for i in range(len(result)):
             if result[i].release_date != '':
-                date_object = datetime.strptime(result[i].release_date, '%Y-%m-%d')
-                string_date = date_object.strftime('%B %d, %Y')
+                date_object = datetime.strptime(result[i].release_date, DATE_UNFORMATTED)
+                string_date = date_object.strftime(DATE_FORMATTED)
                 result[i].release_date = string_date
             
 
--- helpers.py
+++ helpers.py
@@ -7,9 +7,30 @@ def find_similar(movie_title):
     tmdb.api_key = "fb30a4fbe49546ed501bce99179c0fdd"
     tmdb.language = 'en'
     tmdb.debug = True
-
     movie = Movie()
     search = movie.search(movie_title)
-    
 
     return search
+
+def get_trending():
+    tmdb = TMDb()
+    tmdb.api_key = "fb30a4fbe49546ed501bce99179c0fdd"
+    tmdb.language = 'en'
+    tmdb.debug = True
+
+    movie = Movie()
+    popular = movie.popular()
+
+    return popular
+
+def get_upcoming():
+    tmdb = TMDb()
+    tmdb.api_key = "fb30a4fbe49546ed501bce99179c0fdd"
+    tmdb.language = 'en'
+    tmdb.debug = True
+
+    movie = Movie()
+    upcoming = movie.upcoming()
+
+    return upcoming
+    
